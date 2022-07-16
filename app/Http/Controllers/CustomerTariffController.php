acf28f06a857f19358a53954b6e21976904d95bd
Cairo Sales <cairoosf@gmail.com>
2022-07-16 18:15:27 -0300

    :sparkles: feat: inclusão do recurso Tarifas de Clientes

--- app/Http/Controllers/CustomerTariffController.php
+++ app/Http/Controllers/CustomerTariffController.php
@@ -3,62 +3,83 @@
 namespace App\Http\Controllers;
 
 use App\Models\CustomerTariff;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class CustomerTariffController extends Controller
+class CustomerTariffTariffController extends Controller
 {
+ 
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index() : JsonResponse
     {
-        //
+        $customerTariff =  CustomerTariff::paginate(15);
+        return response()->json($customerTariff,200);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(Request $request)
+    public function store(Request $request) : JsonResponse
     {
-        //
+        $customerTariff = new CustomerTariff();
+        $customerTariff->fill($request->all());
+        $customerTariff->save();
+        return response()->json($customerTariff,201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\CustomerTariff  $customerTariff
-     * @return \Illuminate\Http\Response
+     * @param  $id 
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function show(CustomerTariff $customerTariff)
+    public function show($id) : JsonResponse
     {
-        //
+        $customerTariff = CustomerTariff::find($id);
+        if(!$customerTariff){
+            return response()->json('TARIFA NÃO ENCONTRADA',404);
+        }
+        return response()->json($customerTariff,200);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\CustomerTariff  $customerTariff
-     * @return \Illuminate\Http\Response
+     * @param  $id 
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, CustomerTariff $customerTariff)
+    public function update(Request $request, $id) : JsonResponse
     {
-        //
+        $customerTariff = CustomerTariff::find($id);
+        if(!$customerTariff){
+            return response()->json('TARIFA NÃO ENCONTRADA',404);
+        }
+        $customerTariff->fill($request->all());
+        $customerTariff->save();
+        return response()->json($customerTariff,200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\CustomerTariff  $customerTariff
-     * @return \Illuminate\Http\Response
+     * @param  $id 
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy(CustomerTariff $customerTariff)
+    public function destroy($id) : JsonResponse
     {
-        //
+        $customerTariff = CustomerTariff::find($id);
+        if(!$customerTariff){
+            return response()->json('TARIFA NÃO ENCONTRADA',404);
+        }
+        $customerTariff->delete();
+        return response()->json(null,204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::apiResource('customers',[App\Http\Controllers\CustomerController::class])->names('customers');
+Route::apiResource('customers',App\Http\Controllers\CustomerController::class)->names('customers');
+Route::apiResource('customer-tariffs',App\Http\Controllers\CustomerTariffController::class)->names('tariffs');
